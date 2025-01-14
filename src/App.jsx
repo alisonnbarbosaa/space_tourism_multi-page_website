183fa95d4b7710d6a9f9628dd669afafce1de043
alisonnbarbosaa <alisondevbarbosa@gmail.com>
2025-01-14 13:16:29 -0300

    react router adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Home from "./components/pages/Home";
+import Destination from "./components/pages/Destination";
+import Crew from "./components/pages/Crew";
+import Technology from "./components/pages/Technology";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <div className="h-3/4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </div>
       <Footer />
     </BrowserRouter>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,18 +4,18 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <ul>
+        <ul className="flex items-center gap-4">
           <li>
-            <Link>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link>Destination</Link>
+            <Link to="/destination">Destination</Link>
           </li>
           <li>
-            <Link>Crew</Link>
+            <Link to="/crew">Crew</Link>
           </li>
           <li>
-            <Link>Technology</Link>
+            <Link to="/technology">Technology</Link>
           </li>
         </ul>
       </nav>
