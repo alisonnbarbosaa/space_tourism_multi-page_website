86e98b10cccbaf75fd6b9ce0c0dbcf2ac2a764db
alisonnbarbosaa <alisondevbarbosa@gmail.com>
2025-01-18 14:03:03 -0300

    versão tablet do technology

--- src/components/layout/ContainerCrew.jsx
+++ src/components/layout/ContainerCrew.jsx
@@ -2,24 +2,26 @@ import NavBarCrew from "./NavBarCrew";
 
 export default function ContainerCrew({ img, h1, h2, description }) {
   return (
-    <section className="flex h-full w-full max-sm:flex-col-reverse max-sm:items-center max-sm:justify-center">
-      <div className="w-1/2 flex justify-center items-center max-sm:w-full">
-        <div className="w-3/4 h-full flex flex-col justify-evenly max-sm:flex-col-reverse max-sm:w-full max-sm:items-center max-sm:gap-4 max-sm:pt-4">
-          <h3 className="uppercase text-2xl text-white max-sm:hidden">
+    <section className="flex h-full w-full max-sm:flex-col-reverse max-lg:items-center max-lg:justify-center sm:max-lg:justify-between sm:max-lg:flex-col">
+      <div className="w-1/2 flex justify-center items-center max-lg:w-full sm:max-lg:h-1/2">
+        <div className="w-3/4 h-full flex flex-col justify-evenly max-sm:flex-col-reverse max-sm:w-full max-lg:items-center max-lg:gap-4 max-sm:pt-4">
+          <h3 className="uppercase text-2xl text-white max-sm:hidden sm:max-lg:text-left sm:max-lg:w-full sm:max-lg:text-xl">
             <span className="text-[#979797] font-bold pr-2">02</span> Meet your
             crew
           </h3>
-          <div className="flex flex-col gap-4 max-sm:items-center max-sm:text-center">
-            <h2 className="text-[#979797] text-2xl uppercase">{h2}</h2>
-            <h1 className="text-6xl text-white uppercase max-sm:text-4xl">
+          <div className="flex flex-col gap-4 max-lg:items-center max-lg:text-center">
+            <h2 className="text-[#979797] text-2xl sm:max-lg:text-xl uppercase">
+              {h2}
+            </h2>
+            <h1 className="text-6xl text-white uppercase max-sm:text-4xl sm:max-lg:text-4xl">
               {h1}
             </h1>
-            <p className="text-[#979797] text-lg w-3/4">{description}</p>
+            <p className="text-[#979797] text-lg w-3/4 sm:max-lg:w-full">{description}</p>
           </div>
           <NavBarCrew />
         </div>
       </div>
-      <div className="w-1/2 flex justify-center max-sm:border-b-2 border-[#979797]">
+      <div className="w-1/2 sm:max-lg:h-1/2 flex justify-center max-sm:border-b-2 border-[#979797]">
         <img src={img} alt="" className="h-full" />
       </div>
       <h3 className="uppercase text-2xl text-white pb-4 sm:hidden">
--- src/components/layout/ContainerTechnology.jsx
+++ src/components/layout/ContainerTechnology.jsx
@@ -1,34 +1,42 @@
 import NavBarTechnology from "./NavBarTechnology";
 
-export default function ContainerTechnology({ img, imgMobile, h1, h3, description }) {
+export default function ContainerTechnology({
+  img,
+  imgMobile,
+  h1,
+  h3,
+  description,
+}) {
   return (
-    <section className="flex h-full w-full max-sm:flex-col-reverse max-sm:items-center">
-      <div className="w-3/5 flex flex-col items-end max-sm:w-full">
-        <div className="w-4/5 h-3/4 flex flex-col justify-around max-sm:w-full">
-          <h2 className="uppercase text-2xl text-white max-sm:hidden">
+    <section className="flex h-full w-full max-lg:flex-col-reverse max-lg:items-center sm:max-lg:justify-end">
+      <div className="w-3/5 flex flex-col items-end max-lg:w-full">
+        <div className="w-4/5 h-3/4 flex flex-col justify-around max-lg:w-full">
+          <h2 className="uppercase text-2xl text-white max-lg:hidden">
             <span className="text-[#979797] font-bold pr-2">03</span> Space
             launch 101
           </h2>
-          <div className="flex items-center gap-16 max-sm:flex-col">
+          <div className="flex items-center gap-16 max-lg:flex-col sm:max-lg:gap-4">
             <NavBarTechnology />
-            <div className="max-sm:flex flex-col justify-center items-center max-sm:text-center">
+            <div className="max-lg:flex flex-col justify-center items-center max-lg:text-center">
               <h3 className="text-[#979797] text-xl uppercase">{h3}</h3>
-              <h1 className="text-5xl text-white uppercase py-4 max-sm:text-3xl">{h1}</h1>
+              <h1 className="text-5xl text-white uppercase py-4 max-lg:text-3xl">
+                {h1}
+              </h1>
               <p className="text-[#979797] text-lg w-3/4">{description}</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-2/5 max-sm:w-full flex justify-end items-center max-sm:py-4">
+      <div className="w-2/5 max-lg:w-full flex justify-end sm:max-lg:justify-center items-center max-lg:py-4">
         <picture>
-          <source media="(max-width: 640px)" srcset={imgMobile} />
+          <source media="(max-width: 1024px)" srcset={imgMobile} />
           <img src={img} alt="" />
         </picture>
       </div>
-      <h2 className="uppercase text-xl text-white hidden max-sm:block">
-            <span className="text-[#979797] font-bold pr-2">03</span> Space
-            launch 101
-          </h2>
+      <h2 className="uppercase text-xl text-white hidden max-lg:block sm:max-lg:w-4/5">
+        <span className="text-[#979797] font-bold pr-2">03</span> Space launch
+        101
+      </h2>
     </section>
   );
 }
